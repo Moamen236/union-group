0e380f06fe055e7ce258fa8aac21088af2474570
Moamen236 <moamen.ali107@gmail.com>
2026-01-21 00:58:54 +0200

    Update logo path in signin page for improved branding consistency

--- resources/views/admin/pages/auth/signin.blade.php
+++ resources/views/admin/pages/auth/signin.blade.php
@@ -127,7 +127,7 @@
                     @include('admin.components.common.common-grid-shape')
                     <div class="flex max-w-xs flex-col items-center">
                         <a href="/" class="mb-4 block">
-                            <img src="{{ asset('admin/images/auth-logo.png') }}" alt="Logo" />
+                            <img src="{{ asset('admin/images/logo/auth-logo.png') }}" alt="Logo" />
                         </a>
                         <p class="text-center text-gray-400 dark:text-white/60">
                             Union Group is one of Egypt's leading manufacturers of premium sanitary fittings
